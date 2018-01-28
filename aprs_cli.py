92849341095f0b8d921c94ccf72fd68453ee08c3
swilcox <steven@wilcoxzone.com>
2018-01-27 19:53:41 -0600

    first working version of script

---
+++ aprs_cli.py
@@ -0,0 +1,56 @@
+"""APRS Client for BADash"""
+import os
+import sys
+
+import aprslib
+import requests
+
+BADASH_URL = os.environ.get('BADASH_URL', 'http://localhost:8000/events')
+BADASH_API_KEY = os.environ.get('BADASH_API_KEY', '')
+BADASH_JOB = os.environ.get('BADASH_JOB', '')
+
+
+def send_to_badash(packet):
+    """send packet data to badash"""
+    response = requests.post(
+        BADASH_URL,
+        json=packet,
+        headers={'X-Api-Key': BADASH_API_KEY}
+    )
+    print(response)
+
+
+def aprs_callback(packet):
+    """callback for aprs packet being received"""
+    print("received packet: ")
+    print(packet)
+    packet.update(
+        result=0,
+        job=BADASH_JOB,
+        loc={
+            'lng': packet['longitude'],
+            'lat': packet['latitude']
+        },
+    )
+    print("translated packet: ")
+    print(packet)
+    print("sending to BADash")
+    send_to_badash(packet)
+
+
+def main():
+    """the main function"""
+    print("starting listener: ")
+    print(" BADASH_URL: {}".format(BADASH_URL))
+    print(" BADASH_API_KEY: {}".format(BADASH_API_KEY))
+    print(" BADASH_JOB: {}".format(BADASH_JOB))
+    print(" CALL SIGN: {}".format(sys.argv[1]))
+    aprs = aprslib.IS('N0CALL')
+    aprs.set_server('rotate.aprs.net', 14580)
+    aprs.set_filter('p/{}'.format(sys.argv[1]))
+    aprs.connect()
+    aprs.consumer(aprs_callback)
+
+
+if __name__ == '__main__':
+    main()
